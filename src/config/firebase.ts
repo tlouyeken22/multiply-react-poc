b04adea70711fc62ea823f840d0fa8764635d7ea
Tlou Rammala <49203047+tlouyeken22@users.noreply.github.com>
2020-04-13 15:14:28 +0200

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIBxoop3o82-NsE57F-5rsZ7DDOzBL838",
-  authDomain: "momentum-multiply-poc.firebaseapp.com",
-  databaseURL: "https://momentum-multiply-poc.firebaseio.com",
-  projectId: "momentum-multiply-poc",
-  storageBucket: "momentum-multiply-poc.appspot.com",
-  messagingSenderId: "342736840552",
-  appId: "1:342736840552:web:0582ecfac8dd8c49"
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: ...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 firebase.initializeApp(firebaseConfig);
